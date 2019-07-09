3ef54dd3b3d749c98e50e4b5828338c23a85d3e1
Krishnakumar Arjunan <krishnakumar.arjunan@softvison.com>
2019-07-09 17:39:05 +0530

    code clean up for microservice

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ WORKDIR /usr/src/app
 COPY package*.json ./
 RUN npm install
 COPY . .
-# EXPOSE 8000
+EXPOSE 3000
 # CMD [ "node", "server.js"]
 # CMD [ "node --polyglot --jvm", "server.js"]
 CMD node client.js
--- client.js
+++ client.js
@@ -7,12 +7,13 @@ const fs = require('fs');
 const process = require('process');
 const grpc = require('grpc');
 
+
 //Router configuration
 const employeeRouter = require('./routes/employeeRouter')();
 
 //Application port and ip
 const PORT = 3000;
-const HOST = '0.0.0.0';
+// const HOST = '0.0.0.0';
 
 
 // port configuration for GRPC connectivity
@@ -23,6 +24,9 @@ const serviceDef = grpc.load(PROTO_PATH);
 
 const app = express();
 
+const creds = grpc.credentials.createInsecure()
+const client = new serviceDef.EmployeeConsumerService(`localhost:${GRPC_PORT}`, creds);
+
 app.use('/api',employeeRouter);
 
 app.get('/', (req, res) => {
@@ -30,76 +34,13 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT,() => {
+    getAll(client);
     console.log("Running on port " + PORT);
 });
 
-const creds = grpc.credentials.createInsecure()
-const client = new serviceDef.EmployeeConsumerService(`localhost:${GRPC_PORT}`, creds);
 
-const option = parseInt(process.argv[2],10);
 
-switch (option) {
-    case 1:
-        sendMetadata(client);
-        break;
-    case 2:
-        getByBadgeNumber(client);
-        break;
-    case 3:
-        getAll(client);
-        break;
-    case 4:
-        addPhoto(client);
-        break;
-    case 5:
-        saveAll(client);
-        break;
-}
-
-function saveAll(client) {
-    const employees = [
-        {
-			badgeNumber: 123,
-			firstName: "John",
-			lastName: "Smith",
-			vacationAccrualRate: 1.2,
-			vacationAccrued: 0,
-		},
-		{
-			badgeNumber: 234,
-			firstName: "Lisa",
-			lastName: "Wu",
-			vacationAccrualRate: 1.7,
-			vacationAccrued: 10,
-		}
-    ];
-
-    const call = client.saveAll();
-    call.on('data', function (emp) {
-        console.log(emp.employee);
-    });
-    employees.forEach(function (emp) {
-        call.write({employee: emp});
-    });
-    call.end();
-}
-
-function addPhoto(client) {
-    const md = new grpc.Metadata();
-    md.add('badgenumber', '2080');
-
-    const call = client.addPhoto(md, function (err, result) {
-        console.log(result);
-    });
-
-    const stream = fs.createReadStream('Penguins.jpg');
-    stream.on('data', function (chunk) {
-        call.write({data: chunk});
-    });
-    stream.on('end', function () {
-        call.end();
-    });
-}
+// const option = parseInt(process.argv[2],10);
 
 function getAll(client) {
     const call = client.getAll({});
@@ -109,20 +50,4 @@ function getAll(client) {
     });
 }
 
-function sendMetadata(client) {
-    const md = new grpc.Metadata();
-    md.add('username', 'krishna');
-    md.add('password', 'kumar');
 
-    client.getByBadgeNumber({}, md, function () {});
-}
-
-function getByBadgeNumber(client) {
-    client.getByBadgeNumber({badgeNumber: 2080}, function (err, response) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log(response.employee);
-        }
-    });
-}
